451936015b9525702e333a6f85332cca0869d774
camperfail86 <mega.kochkin@inbox.ru>
2023-10-29 10:24:32 +0500

    Сортировка по алфавиту

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -8,9 +8,11 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
     switch (action.type) {
         case 'sort': { // by name
             if (action.payload == 'up') {
-                return [...state.sort((a, b) => b.age - a.age )]
+                return [...state.sort((a, b) =>
+                    a.name > b.name ? -1 : a.name < b.name ? 1 : 0 )]
             } else {
-                return [...state.sort((a, b) => a.age - b.age )]
+                return [...state.sort((a, b) =>
+                    a.name < b.name ? -1 : a.name > b.name ? 1 : 0 )]
             }
         }
         case 'check': {
