016a464f95080575fc94daf72ded866506e35a9f
camperfail86 <mega.kochkin@inbox.ru>
2023-10-29 10:25:58 +0500

    1

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,7 +7,7 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-            if (action.payload == 'up') {
+            if (action.payload == 'down') {
                 return [...state.sort((a, b) =>
                     a.name > b.name ? -1 : a.name < b.name ? 1 : 0 )]
             } else {
