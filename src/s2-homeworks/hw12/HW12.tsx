d0c16706629b257f3769dfccc964b4eb816b7992
camperfail86 <mega.kochkin@inbox.ru>
2023-11-20 16:55:21 +0500

    hw12

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -3,7 +3,8 @@ import s from './HW12.module.css'
 import s2 from '../../s1-main/App.module.css'
 import SuperSelect from '../hw07/common/c5-SuperSelect/SuperSelect'
 import {useDispatch, useSelector} from 'react-redux'
-import {changeThemeId} from './bll/themeReducer'
+import {changeThemeId, themeReducer} from './bll/themeReducer'
+import {AppStoreType} from "../hw10/bll/store";
 
 /*
 * 1 - в файле themeReducer.ts написать нужные типы вместо any, дописать редьюсер
@@ -19,11 +20,12 @@ const themes = [
 ]
 
 const HW12 = () => {
+    const dispatch = useDispatch()
     // взять ид темы из редакса
-    const themeId = 1
-
-    const change = (id: any) => { // дописать функцию
-
+    const themeId = useSelector<AppStoreType, number>(state => state.theme.themeId)
+    const change = (id: number) => { // дописать функцию
+        console.log(id)
+        dispatch(changeThemeId(id))
     }
 
     useEffect(() => {
@@ -40,8 +42,10 @@ const HW12 = () => {
                 <SuperSelect
                     id={'hw12-select-theme'}
                     className={s.select}
+                    options={themes}
                     // сделать переключение тем
-
+                    onChangeOption={change}
+                    // onChange={change}
                 />
             </div>
         </div>
